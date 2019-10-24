8c892cc4dac10c7a05c3cbf4cbdf8a86b87fcf30
Benedikt89 <constantine.kosta@gmail.com>
2019-10-24 21:02:20 +0300

    Updates

---
+++ precache-manifest.0d6ecc118041f25ed12a85e3f591f57f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644985cbe45f2069bba7bc9db1b48dc1",
+    "url": "/dog-game/index.html"
+  },
+  {
+    "revision": "c4fcb39666510ca98737",
+    "url": "/dog-game/static/css/main.f1ff900c.chunk.css"
+  },
+  {
+    "revision": "6041fc030e2074cdcbe8",
+    "url": "/dog-game/static/js/2.f7aef9b6.chunk.js"
+  },
+  {
+    "revision": "c4fcb39666510ca98737",
+    "url": "/dog-game/static/js/main.6c2d7d4c.chunk.js"
+  },
+  {
+    "revision": "c7a880bb4e1ff57bd6f4",
+    "url": "/dog-game/static/js/runtime-main.2fd001ae.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dog-game/static/media/dog.75fa68d4.mp3"
+  }
+]);
